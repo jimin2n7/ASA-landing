5f4fcb1a9f282c1af6f07e63d04796a93d55533f
killerburn69 <kietthetran123@gmail.com>
2024-04-03 15:42:16 +0700

    setup aos

--- src/pages/Home/components/Header/Hero.tsx
+++ src/pages/Home/components/Header/Hero.tsx
@@ -9,15 +9,15 @@ const Hero = () => {
     <div className='bg-img'>
         <Layout>
             <div className='grid grid-cols-5 h-[700px]'>
-                <div className='col-span-3 flex items-center'>
+                <div className='col-span-3 flex items-center' data-aos="fade-right">
                     <Introduce/>
                 </div>
-                <div className='col-span-2 flex items-center z-10'>
+                <div className='col-span-2 flex items-center z-10' data-aos="fade-left">
                     <ImageHeader/>
                 </div>
             </div>
         </Layout>
-        <div>
+        <div data-aos="fade-up">
             <MainSupport/>
         </div>
     </div>
--- src/pages/Home/components/Header/ImageHeader.tsx
+++ src/pages/Home/components/Header/ImageHeader.tsx
@@ -3,8 +3,7 @@ import CardNFT from '~/components/CardNFT'
 
 const ImageHeader = () => {
   return (
-    <div className='relative'>
-
+    <div className='purple_bg relative'>
       <CardNFT/>
       <div className='absolute top-0 right-0 translate-x-1/3 translate-y-[20%] z-[-1]'>
         <CardNFT/>
