253abecd6d61c8cac28d6cf642254e5b948b3c4e
jimin2n7 <nguyenchiminhnt85@.com>
2024-04-03 15:20:00 +0700

    scroll

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,16 @@ import { store } from '~/store'
 import { Provider } from 'react-redux'
 import Navbar from './components/Navbar'
 import Layout from './components/Layout'
+import { ScrollTop } from './components/ScrollToTop'
 
 function App() {
   return (
     // <Provider store={store}>
     <Suspense fallback={<div>Loading...</div>}>
       <BrowserRouter>
-        
-          <Navbar/>
-          <MainView />  
-        
+        <Navbar />
+        <MainView />
+        <ScrollTop />
       </BrowserRouter>
     </Suspense>
     // </Provider>
